856fb8a2c552bef19885f5acc5c052d8fc0dd471
onclave <11374405+onclave@users.noreply.github.com>
2021-02-26 04:32:49 +0530

    update chrome-driver to be managed by chrome-driver-manager

--- portia_config/config.py
+++ portia_config/config.py
@@ -1,11 +1,36 @@
-_CHROME_DRIVER_DESTINATION: str = "/Volumes/Vault/Developer/webdriver/chromedriver"
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+_WEBDRIVER_OPTIONS: Options = Options()
+_WEBDRIVER_OPTIONS.add_argument('--headless')
+_WEBDRIVER: WebDriver = webdriver.Chrome(
+    ChromeDriverManager(
+        cache_valid_range=1,
+        log_level=0
+    ).install(),
+    options=_WEBDRIVER_OPTIONS
+)
 
 
-def get_chrome_drive_destination() -> str:
-    global _CHROME_DRIVER_DESTINATION
-    return _CHROME_DRIVER_DESTINATION
+def get_chrome_driver() -> WebDriver:
+    global _WEBDRIVER
+    return _WEBDRIVER
 
 
-def set_chrome_drive_destination(dest: str):
-    global _CHROME_DRIVER_DESTINATION
-    _CHROME_DRIVER_DESTINATION = dest
+def set_chrome_driver(
+        cache_valid_range: int,
+        options: Options,
+        log_level: int
+):
+
+    global _WEBDRIVER
+
+    _WEBDRIVER = webdriver.Chrome(
+        ChromeDriverManager(
+            cache_valid_range=cache_valid_range,
+            log_level=log_level
+        ).install(),
+        options=options
+    )
