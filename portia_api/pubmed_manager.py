7f7ea167d11afca1c9c3053b13d4576ef1b39c4b
onclave <11374405+onclave@users.noreply.github.com>
2021-03-05 04:42:56 +0530

    add additional keyword support in PubMed api

--- portia_api/pubmed_manager.py
+++ portia_api/pubmed_manager.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing import Iterator
 from portia_types.pubmed_article import ArticleDictionary
 from web_scrapper.pubmed_scrapper import PubMedScrapper
 
@@ -10,15 +11,42 @@ class PubmedManager:
     def __init__(self):
         self._scrapper = PubMedScrapper()
 
-    def fetch_result(
+    def fetch(
             self,
-            data: str,
+            search_term: str,
             get_citation: bool = False
     ) -> List[ArticleDictionary]:
-
         pubmed_result: List[ArticleDictionary] = self._scrapper.get_search_result(
-            data,
+            search_term,
             get_citation
         )
 
         return pubmed_result
+
+    def fetch_with_associated_keywords(
+            self,
+            search_term: str,
+            keywords: List[str],
+            get_citation: bool = False
+    ) -> List[ArticleDictionary]:
+        results: List[ArticleDictionary] = self.fetch(search_term, get_citation=get_citation)
+        filtered: Iterator[ArticleDictionary] = filter(
+            lambda item: PubmedManager.filter_by_keyword(item, keywords),
+            results
+        )
+        return [article for article in filtered]
+
+    def fask(
+            self,
+            search_term: str,
+            keywords: List[str],
+            get_citation: bool = False
+    ) -> List[ArticleDictionary]:
+        return self.fetch_with_associated_keywords(search_term, keywords, get_citation=get_citation)
+
+    @staticmethod
+    def filter_by_keyword(article: ArticleDictionary, keywords: List[str]):
+        for word in keywords:
+            if word in article['title'] or word in article['abstract']:
+                return True
+        return False
